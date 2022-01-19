6b0bbc0a3563a1c8cf44a252b670181a91f0ed35
Matija Marohnić <matija.marohnic@gmail.com>
2022-01-19 16:18:18 +0100

    Set up testing

--- test/toast.test.tsx
+++ test/toast.test.tsx
@@ -1,12 +1,58 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Toaster, toast } from '../src';
+import React from 'react';
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+  act,
+} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import MatchMediaMock from 'jest-matchmedia-mock';
+import toast, { Toaster } from '../src';
 
-describe('it', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    toast('Hello World');
-    ReactDOM.render(<Toaster />, div);
-    ReactDOM.unmountComponentAtNode(div);
-  });
+let matchMedia: MatchMediaMock;
+
+beforeAll(() => {
+  matchMedia = new MatchMediaMock();
+  matchMedia.useMediaQuery('(prefers-reduced-motion: reduce)');
+});
+
+afterAll(() => {
+  matchMedia.destroy();
+});
+
+jest.useFakeTimers()
+
+test('closes notification', async () => {
+  render(
+    <>
+      <Toaster />
+      <button
+        type="button"
+        onClick={() => {
+          toast((t) => (
+            <div>
+              Example
+              <button
+                type="button"
+                onClick={() => toast.dismiss(t.id)}
+                title={'close'}
+              >
+                Close
+              </button>
+            </div>
+          ));
+        }}
+      >
+        Notify!
+      </button>
+    </>
+  );
+  userEvent.click(screen.getByRole('button', { name: /notify/i }));
+  screen.getByText(/example/i);
+
+  userEvent.click(screen.getByRole('button', { name: /close/i }));
+  act(() => {
+    jest.runAllTimers()
+  })
+  await waitForElementToBeRemoved(() => screen.queryByText(/example/i));
 });
